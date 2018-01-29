ce5972bfa630095f517d4a5ed44b8c2b2b57a049
Ben Casalino <bencasalino@gmail.com>
2018-01-29 11:51:10 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./App.css";
+import  Header  from "./Header";
+import { JobDetails } from "./JobDetails";
+import  Footer  from "./Footer";
+import { InputForm } from "./InputForm";
+import { Preview } from "./Preview";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("./listing.json")
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          data: response
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <head>
+        <title> heyyyoooo</title>
+        </head>
+        <Header />
+        <main>
+          <JobDetails jobData={this.state.data} />
+          <InputForm />
+          <Preview />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header(){
+  return (
+     <header>
+        <img src="./assets/g-dino.png" alt="Dinosaur Logo" />
+     </header>
+  )
+  
+}
+
+
+
+
+
